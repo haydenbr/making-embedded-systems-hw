5e47257a18cb72c11babf9f50ea6a6a58e69b5c4
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2022-05-07 11:38:26 -0400

    attempting to get interrupts working but still having trouble

--- week3/src/main.cpp
+++ week3/src/main.cpp
@@ -1,15 +1,16 @@
 #include "main.hpp"
 
 int main() {
-  // enable clock for GPIOA port
+  // enable clock for GPIO A port
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   // button pin input mode
-  GPIOA->MODER &= ~(0x3 << (BUTTON_PIN*2));
+  // GPIOA->MODER &= ~(0x3 << (BUTTON_PIN*2));
+  GPIOA->MODER = 0x00000000;
   // button pin pull-down
-  GPIOA->PUPDR &= ~(0x3 << (BUTTON_PIN*2));
-  GPIOA->PUPDR |= (0x2 << (BUTTON_PIN*2));
+  // GPIOA->PUPDR &= ~(0x3 << (BUTTON_PIN*2));
+  // GPIOA->PUPDR |= (0x2 << (BUTTON_PIN*2));
 
-  // enable clock for GPIOG port
+  // enable clock for GPIO G port
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
   // write 00 (reset) for LED pin mode
   GPIOG->MODER  &= ~(0x3 << (LED_PIN*2));
@@ -18,13 +19,29 @@ int main() {
   // write 0 (output push-pull) for LED pin output type
   GPIOG->OTYPER &= ~(1 << LED_PIN);
 
-  // bool button_pressed = 0;
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+  SYSCFG->EXTICR[0] |= 0x00;
+
+  // enable rising edge trigger
+  EXTI->RTSR |= (1 << BUTTON_PIN);
+  // unmask interrupt
+  EXTI->IMR |= (1 << BUTTON_PIN);
+
+  NVIC_SetPriority(EXTI0_IRQn, 1);
+  NVIC_EnableIRQ(EXTI0_IRQn);
+
   while (1) {
-    unsigned int idr_val = GPIOA->IDR;
-    if (idr_val & (1 << BUTTON_PIN)) {
+    if (led_on) {
       GPIOG->ODR |= (1 << LED_PIN);
     } else {
       GPIOG->ODR &= ~(1 << LED_PIN);
     }
+  }
+}
+
+void EXTI0_IRQHandler(void) {
+  if (EXTI->PR & (1 << BUTTON_PIN)) {
+    EXTI->PR |= (1 << BUTTON_PIN);
+    led_on = true;
   }
 }
--- week3/src/main.hpp
+++ week3/src/main.hpp
@@ -1,11 +1,14 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-// #include "stm32f767xx.h"
 #include "stm32f429xx.h"
 #include <cstdint>
 
 constexpr std::uint8_t BUTTON_PIN = 0; // PA0
 constexpr std::uint8_t LED_PIN = 13; // PG13
+
+volatile bool led_on = false;
+
+void EXTI0_IRQHandler(void);
 
 #endif
